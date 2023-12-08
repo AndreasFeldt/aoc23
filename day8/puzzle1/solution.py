5048653767b53b2f3d6d07ed1fa8e1abc224ad2d
Andreas <andreas@feldt.io>
2023-12-08 16:05:51 +0100

    Cleaned code

--- day8/puzzle1/solution.py
+++ day8/puzzle1/solution.py
@@ -1,24 +1,9 @@
-from tqdm import tqdm
 from termcolor import colored
 from itertools import cycle
-# input_text = """
-# LLR
-# AAA = (BBB, BBB)
-# BBB = (AAA, ZZZ)
-# ZZZ = (ZZZ, ZZZ)
-# """.strip().splitlines()
+from halo import Halo
 
-# input_text = """
-# RL
 
-# AAA = (BBB, CCC)
-# BBB = (DDD, EEE)
-# CCC = (ZZZ, GGG)
-# DDD = (DDD, DDD)
-# EEE = (EEE, EEE)
-# GGG = (GGG, GGG)
-# ZZZ = (ZZZ, ZZZ)
-# """.strip().splitlines()
+
 
 input_text = open('day8/input.txt', 'r').read().strip().splitlines()
 
@@ -27,12 +12,10 @@ def find_path(instr: str, ways: dict):
     ways = dict(sorted(ways.items()))
     instr = cycle(instr)
     b = list(ways.keys())[0]
-    print(b)
     while b != 'ZZZ':
         LR = next(instr)
         b = ways[b][LR]
         n+=1
-        print(n)
     return n
 
 def make_new_map(_map) -> dict:
@@ -50,8 +33,11 @@ def main() -> str:
     _map = input_text
     input_text.pop(0)
     _map = make_new_map(_map)
-    print(colored("* Processing", 'red'))
-    return find_path(instr, _map)
+    with Halo(text=colored("Processing paths", 'red'), spinner='dots') as spinner:
+        data = find_path(instr, _map)
+        spinner.succeed(text=colored('All paths processed!', 'green'))
+        return data
     
 if __name__ == "__main__":
-    print(colored(f"* Number of steps to ZZZ: {str(main())}", "light_green"))
+    with Halo(text="Processing", spinner='dots') as aa:
+        aa.succeed(colored(f"Number of steps to ZZZ: ", "cyan")+ f"{colored(str(main()), 'red')}")
